0aa9d138582246fdb3118e604664c78d058d8bca
cutetrick <tngkrdms8@gmail.com>
2024-05-29 18:06:54 +0900

    .

---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# javaStudy
+java study 20240529
+=======
+# GithubExam
+깃헙 연동 테스트
+>>>>>>> refs/remotes/origin/main
---
+++ src/test.java
@@ -0,0 +1,10 @@
+package src;
+
+public class tests {
+
+	public static void main(String[] args) {
+
+		System.out.printnl("test");
+	}
+
+}
